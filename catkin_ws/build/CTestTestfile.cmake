a7205f844626616208d3293e6c4dafa253ffa15c
Radmilase <radmila@example.com>
2025-06-08 12:23:05 +0300

    1

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/rad/robot_programming/catkin_ws/src
+# Build directory: /home/rad/robot_programming/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("master_scene")
+subdirs("robotiq_85_simulation/robotiq_85_gazebo")
+subdirs("robotiq_85_simulation/robotiq_85_simulation")
+subdirs("robotiq_85_description")
+subdirs("solution_master")
+subdirs("gazebo_grasp_plugin")
+subdirs("roboticsgroup_gazebo_plugins")
+subdirs("ur_description")
+subdirs("ur_perception")
---
+++ catkin_ws/build/solution_master/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rad/robot_programming/catkin_ws/src/solution_master
+# Build directory: /home/rad/robot_programming/catkin_ws/build/solution_master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
